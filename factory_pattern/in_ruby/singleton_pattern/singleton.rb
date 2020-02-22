83dc551486c812f1c0b366e3d956a173b6846338
Bryan Ramos <b.r.tech95@gmail.com>
2020-02-22 05:33:30 -0500

    singleton pattern implementation complete as well as notes for the pattern

---
+++ factory_pattern/in_ruby/singleton_pattern/singleton.rb
@@ -0,0 +1,40 @@
+class President
+  
+   
+    private
+    @@instance 
+
+    public
+
+    def initialize
+      raise "multiple instances cannot be created"
+    end
+   
+
+   #singleton methods 
+  class << self
+       
+    attr_accessor :instance
+    
+    def get_instance
+      if self.instance.nil?
+        self.instance = self.class.new
+      end
+    end
+
+    def clone
+      raise "Object cannot be cloned"
+    end
+
+  end
+end
+
+president = President.get_instance
+
+doppleganger = president
+
+puts doppleganger == president #returns true since only one object of this class is created.
+
+president2 = President.new #Raises error since there can only be one of this object.
+
+
